288b3a42a265f8d9e73b011bca426a5d130654ae
JITHA JAYESH <jitha210806@gmail.com>
2026-07-21 15:32:33 +0530

    Create config.py

---
+++ config.py
@@ -0,0 +1,9 @@
+# ============================
+# ThingSpeak Configuration
+# ============================
+
+WRITE_API_KEY = "CG12P2D7N6PES2DA"
+
+URL = "https://api.thingspeak.com/update"
+
+UPDATE_INTERVAL = 15
